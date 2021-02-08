5a550b7cb12e516ac8b0e7ed4d34c6173cd2cab9
botan <botan.david94@gmail.com>
2021-02-08 15:34:52 +0100

    final project

--- mainapp/run.py
+++ mainapp/run.py
@@ -1,20 +1,41 @@
 from flask import (Flask, request, render_template)
 import requests
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
+IP = "localhost"
+PORT = 3000
 
+def build_url(route):
+    return F"http://{IP}:{PORT}/{route}"
+
+def build_html_url(route, name):
+    route = build_url(route)
+    return F"<a href={route} target='_blank'>{name}</a>"
+    
 @app.route("/")
 def hello():
-    return "Running"
+    return F"Hello from this super app, list of available routes:<br>{build_html_url('load/s3', 'load data from s3')}" \
+                F"<br>{build_html_url('load/rds', 'load data from rds')}" \
+                F"<br>{build_html_url('clear', 'clear rds data')}" \
+                F"<br>{build_html_url('transfert/rds', 'transfert s3 data to rds')}" 
+                
+@app.route("/load/s3")
+def loadS3():
+    return requests.get(url=build_url("/load/s3")).text
+    
+@app.route("/load/rds")
+def loadRDS():
+    return requests.get(url=build_url("/load/rds")).text
+    
+@app.route("/clear")
+def clearRDS():
+    return requests.get(url=build_url("/clear")).text
 
-
-@app.route("/content")
-def content():
-    # Todo 1
-    response = requests.get(url="http://XXXXX:3000/content")
-    return render_template("content.html", response=response.text)
+@app.route("/transfert/rds")
+def transfertToRDS():
+    return requests.get(url=build_url("/transfert/rds")).text
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3001)
--- secondapp/run.py
+++ secondapp/run.py
@@ -1,44 +1,70 @@
 from flask import (Flask, request, Response, render_template, make_response, jsonify)
 import boto3
+import mysql.connector
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    """GET in server"""
-    response = Response("Running")
-    # Enable Access-Control-Allow-Origin
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
-
-
-@app.route("/content")
-def content():
+@app.route("/load/s3")
+def loadS3():
+    matiere = ''
+    if 'matiere' in request.args:
+        matiere = request.args['matiere']
     s3 = S3()
-    result = s3.load()
+    result = s3.load(matiere)
     response = Response(result)
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
+@app.route("/load/rds")
+def loadRDS():
+    matiere = ''
+    if 'matiere' in request.args:
+        matiere = request.args['matiere']
+    rds = RDS()
+    result = rds.load(matiere)
+    response = Response(result)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+    
+@app.route("/clear")
+def clearRDS():
+    rds = RDS()
+    result = rds.clear()
+    response = Response("Clear")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+    
+@app.route("/transfert/rds")
+def transfertToRDS():
+    s3 = S3()
+    result = s3.load('')
+    
+    rds = RDS()
+    rds.insert(result)
+    response = Response("Success")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response  
+
 
 class S3:
     def __init__(self):
-        # ToDo 2
         self.s3 = boto3.client('s3',
-                               aws_access_key_id='XXXXX',
-                               aws_secret_access_key='XXXXX')
+                               aws_access_key_id='AKIAWEWNDMY3VAXBGPJM',
+                               aws_secret_access_key='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')
 
-    def load(self):
-        # ToDo 3
+    def load(self, filter):
+        if(filter == ''):
+            request = "SELECT * FROM s3object s"
+        else:
+            request = F"SELECT * FROM s3object s WHERE s._1 LIKE '%{filter}%'"
         response = self.s3.select_object_content(
-            Bucket='XXXXX',
-            Key='XXXXX',
+            Bucket='projet-final',
+            Key='notes-ges.csv',
             ExpressionType='SQL',
-            Expression="SELECT s.Index FROM s3object s where (CAST(s.Height as FLOAT) > 75) and (CAST(s.Height as FLOAT) < 80 )",
+            Expression=request,
             InputSerialization={
                     'CSV': {
-                        'FileHeaderInfo': 'USE',
                         'RecordDelimiter': '\n',
                         'FieldDelimiter': ',',
                     }
@@ -55,10 +81,53 @@ class S3:
 
         for event in response['Payload']:
             if 'Records' in event:
-                records.append(event['Records']['Payload'])
+                records.append(event['Records']['Payload'].decode('utf-8'))
             elif 'Stats' in event:
                 stats = event['Stats']['Details']
-        return records[0]
+        return records
+        
+class RDS:
+
+    def __init__(self):
+        self.cnx =  mysql.connector.connect(user='admin', password='adminadmin',
+                          host='database-projet-final.cr3gygd34o8b.us-east-1.rds.amazonaws.com',
+                          database='notes')
+ 
+    def clear(self):
+        cursor = self.cnx.cursor()
+        cursor.execute("delete from notes")
+        self.cnx.commit()
+        return 
+        
+    def insert(self, data):
+        cursor = self.cnx.cursor()
+        add_query = ("INSERT INTO notes "
+                   "(matiere, intervenant, coef, ects, CC1, CC2) "
+                   "VALUES (%s, %s, %s, %s, %s, %s)")
+        for i in data[0].split("|"):
+            if(len(i) == 0):
+                continue
+            split_data = i.split(",")
+            print(split_data)
+            insert_data = (split_data[0], split_data[1], split_data[2], split_data[3], split_data[4], split_data[5])
+            print(insert_data)
+            cursor.execute(add_query, insert_data)
+        self.cnx.commit()
+        return
+    
+    def load(self, filter):
+        
+        cursor = self.cnx.cursor()
+        if(filter == ''):
+            cursor.execute("SELECT * FROM notes")
+        else:
+            cursor.execute("SELECT * FROM notes WHERE matiere LIKE '%{filter}%'")
+        
+        data = []
+        for (matiere, intervenant, CC1, CC2, coef, ects) in cursor:
+            data.append(F"{matiere} - {intervenant} - {coef} - {ects} - {CC1} - {CC2}<br>")
+            
+        return "Empty" if len(data) == 0 else data
 
 
 if __name__ == "__main__":
